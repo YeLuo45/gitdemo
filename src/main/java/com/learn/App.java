f499b5c44db3a58d6f2e5e84ee6eb77c582bacc5
YeLuo45 <1466356099@qq.com>
2018-11-27 11:19:01 +0800

    制造冲突并提交到远程Git分支仓库

--- src/main/java/com/learn/App.java
+++ src/main/java/com/learn/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World! edit by idea" );
+    }
+
+    public static void methodB()
+    {
+        System.out.println( "method by idea" );
     }
 }
